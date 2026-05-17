8fddeabf7c448c92c4a36408d2cab91f90996785
Nada Imadache <121617189+notnada@users.noreply.github.com>
2026-05-17 21:13:04 +0100

    Add files via upload

---
+++ Hiraeth.py
@@ -0,0 +1,301 @@
+
+#A list of the game's visual elements.
+
+vis = ["""
+                                               %%%%%%%%%%%%%%%%@@@@@%@@@%%%%%%%%%%%%%%%%%%%%%%%%#####*=====-::::::-::
+                                               %%%%%%%%%%%%%%%%@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%###**++--::::::-::
+                                               %%%%%%@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%##########*+=-::-:
+                                               %@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%#%############*+=
+                                               @@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%###############
+                                               @@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%##############
+                                               @@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%##############
+                                               %@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%###########*
+                                               %#%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%######*+=-::
+                                               %#%%#%#%%%%%%%%%%@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#*++=--:-:::::
+                                               #%%##%#%#%%%%#%#%####%%%%%%%%%%%%%%%%%%%%%%%%%%%#######**-::::::-:::::
+                                               ##%####%#%%%%##%#####%%#####**************#****###*****+=:::-::::-::::
+                                               ######%%%%%#%###########***##***********###*******+==**+-::::::::-::::
+                                               ######%%%%%%%%#########************************+=---=++=:-:::-:-::-:::
+                                               ###%#%%%%%%%%%%########*******************+++=------+++-::::::=++:-:::
+                                               **#%%%%%%%%#%%##########********+++++====----------=+++=::-::-=+=-:-::
+                                               **%%%%%%%%%%%%#*%#######+====----------------------+++++-::--**+*+--:: 
+                                               ##%%%%%%%%%%%%###%%#####*==--------------------++-=++++++=-=*+***++:-:
+                                               ###%%%%%%%%%*%%%##%###%%#====----------------=##-=*+++=====*+#***==:-:
+                                               #%#%%%%%%%%+#%###*#####%%#==---------------+#%*-=##++==++==++##**=-:--
+                                               ***%%*#%%%%%%########%###@%#+=---------=+#%%#=-=##*===+%##++*%*+##+=**
+                                               ##%%%**%%%%%%%%######%%#*#%@@%#*++++*#%%@%%+--=###*===+%%%###%+**#*+**
+                                               %#%%#**%%##%%%############*#%@@@@@@@@@@%#+--===***++++#%%%##%*########
+                                               %%##%#%%%#%#%%########*#**#**+*#%%%%%#+=--=++-++++****%%%%###=#%######
+                                               ######%%%%%#%%########******#*+=--------=+*+=-=+*#*++*%%%####=*%###***
+                                               ######%%%%%#%%%%%%%%%%**==++****+----=+*+==---+#%###%%%%%%***+#%######
+                                               #######%%%%%%%%%%%%@@##*+==-=+****++++++=----=*#%%%#%%%%%#+=++#%#####*
+                                               ########%@%%%@@@%%@@%#%%%==+++++***+*++++*+=-%%%*%%%#%%%#%%##*####**##
+                                               %#%%%%@@@@%%@@@%%@@@#*##@#%%%##***#*+*#%%%%%#%#*+#%%##%%%#%%%%%%%%%%%%
+                                               @@@@@@@@@%%@@@%%%@@%%%@@@%%%%%%%%%%%%%%%%%%%%%%%%#%%%##%%%#%%%%%%%%%%%
+                                               @@@@@@@@%%@@@@%%@@%##%@@@@%%%%%%%%%%%@@%%%%%%%%%%##%%##%%%##%%%%%%%%%%
+                                               @@@@@@@@%@@@@%%%@@#**##%@@@%%%%%%%%%%@@@%%%%%%#*++*%%###%%%#%%%%%%%%%%
+                                               @@@@@@@%%@@@%%%@@%#******#%%%#***+*+*+*####**++++++#%%###%%%#%%%%%%%%%
+                                               @@@@@@@%%@@@%%@@@#***************++++++++++++++++++*%%###%%%%#%%%%%%%%
+                                               @@@@@@@%@@@%%%@@%#**#*****#******+++++++++++++++==+*%%###%%%%%%%%%%%%%\n """, 
+
+
+""" =======================================================================================================================================================================
+                                                                   ┌──*̥˚──◌──◌──*̥˚─┐
+                                                                        Congrats!
+                                                                   └◌───*̥˚───◌───*̥˚┘
+
+
+ You have reached the end of this game. Hope you found some bugs for me (or not) :))
+
+                                                                                                                                                      
+                                        ██████              
+            ██████                    ████▒▒████            
+          ████▒▒████████████████████████████▒▒██            
+        ██░░████▒▒██░░░░░░░░░░░░░░░░░░░░████▒▒████          
+      ██░░░░░░████▒▒██░░░░░░░░░░░░██░░░░░░░░████▒▒██        
+      ██░░░░░░██▒▒██░░░░░░░░░░░░░░░░██░░░░░░░░██▒▒██        
+    ██░░░░░░██▒▒██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████        
+    ██░░░░░░░░██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██        
+  ██░░░░░░████░░░░░░██░░░░░░░░░░░░░░██░░░░░░░░  ░░██        
+  ██░░░░░░██░░░░░░██░░░░░░░░░░░░░░░░██░░      ░░░░░░██      
+  ██░░░░░░██░░░░░░██░░      ░░    ██  ██░░░░░░░░░░░░██      
+██░░░░░░░░██░░░░░░██░░░░░░░░██░░░░██  ████░░░░██░░░░██      3yit wlh 3yit
+██░░░░░░░░██░░░░██░░░░░░░░██░░░░██      ██░░░░░░██░░██      
+██░░░░░░░░██░░░░██░░░░░░██░░░░░░██    ██████░░░░████        
+██░░░░░░░░██░░████░░░░██████████      ██  ██░░░░████        
+██░░░░░░░░██░░████████  ████  ██      ▒▒  ██░░░░██          
+██░░░░░░░░████████▒▒░░    ▒▒          ░░  ██░░████████  ██  
+██░░░░░░░░████░░██▒▒░░  ▒▒░░          ▒▒  ██░░████  ████  ██
+██░░░░░░░░██  ████▒▒░░  ▒▒▒▒              ████░░██  ██    ██
+██░░░░░░░░██    ██░░░░                  ████░░░░██      ██  
+██░░░░░░░░██      ████░░            ██████░░░░░░██    ██    
+██░░░░░░░░░░██        ██▓▓▓▓▓▓▓▓▓▓░░▓▓  ██░░░░██    ██      
+██░░░░░░░░░░██      ██    ▓▓▓▓▓▓▓▓░░▓▓██░░████    ██        
+██░░░░░░░░░░██    ██░░░░██▓▓▓▓▓▓▓▓░░▓▓██████    ████        
+██░░░░░░░░░░██  ████████░░░░▓▓▓▓░░██░░▓▓██    ██░░░░██      
+██░░░░░░░░░░░░██████████████░░░░██████░░██  ██░░░░░░██      
+  ██░░░░░░░░░░████  ████████████████████▓▓██░░░░░░░░██      
+  ██░░░░░░░░░░██  ████░░░░░░░░░░░░░░░░░░████░░░░░░░░██      
+    ██░░░░░░░░██      ██████████████████    ██░░░░██        
+      ██░░░░██      ████████      ██░░██      ████          
+        ████        ██░░██          ████                    
+                      ██                                    
+
+⠀⠀⠀
+
+"""]
+
+
+
+
+
+
+
+
+
+
+#Making a dictionary of the game's rooms.
+worldrooms= {
+
+       "town square" : 
+
+      {
+            "desc" : """ You open your eyes in Town square. The town square is a large open space with no sign of life to be seen.\n It is surrounded with shops which seem to be closed except for one.""" ,
+      "wizard corner" : "\n A small old shop stands in front of you.'Wizard Corner' is written on its door." ,
+      "paper" : "\n A torn piece of paper rests on the ground beside you.",
+      "actions" : "\n > look paper                > Wizard Corner"
+                                                                                },
+
+
+    "wizard corner" : 
+    { 
+      "desc" :''' The door behind you closes, it seems like there is no going back.\n A masked shop owner greets you.''',
+
+       "talk" : '''                                              "Welcome O lost soul ! Pick a card and lets see what fate has for you."\n
+                                                                                  [♥]  [♠]      
+                                                                                     ''' ,            
+
+             
+             "actions1" : "> Can you help me find home?\n> Heart\n> Spade" , 
+             "actions2": "> Run away"
+}
+
+}
+
+#Definig functions.
+
+def show_location(loc):
+      print('\n\n\n')
+      print(loc.title())
+      print("=" * len(loc))
+      print()
+      if loc == "town square" :
+            print(worldrooms[loc]["desc"])
+            print()
+            print(worldrooms[loc]["wizard corner"])
+            print()
+            print(worldrooms[loc]["paper"])
+            print()
+            print(worldrooms[loc]["actions"])
+            print('\n\n\n\n\n\n\n')
+      elif loc == "wizard corner" :
+            print(worldrooms["wizard corner"]["desc"])
+            print(vis[0])
+            print(worldrooms["wizard corner"]["talk"])
+            print (worldrooms["wizard corner"]["actions1"])
+     
+
+def movedirection(direction):
+
+    global loc
+
+    if loc == "town square" and direction == "wizard corner":
+           print()
+           print (f'You move to the {direction}.\n')
+           loc = direction
+           show_location(loc)
+           
+    else:
+           print("You cannot move in that direction\n")
+
+def try_again() :
+    while True :
+     re = input("try again? y/n\n> ")
+     if re == "n" : 
+        re = input("are you sure? all progress will be lost. y/n\n> ")
+        if re == "y" :
+            quit()
+        elif re == "n":
+           return True
+        else :
+           continue
+     elif re == "y" :
+           return True
+     else :
+        print("Type y or n please")
+        
+
+# The welcome screen
+
+print(''' 
+   WELCOME TO HIRAETH DEMO!
+  ==========================
+
+                                                ▒█░▒█ ░▀░ █▀▀█ █▀▀█ █▀▀ ▀▀█▀▀ █░░█ 
+                                                ▒█▀▀█ ▀█▀ █▄▄▀ █▄▄█ █▀▀ ░░█░░ █▀▀█ 
+                                                ▒█░▒█ ▀▀▀ ▀░▀▀ ▀░░▀ ▀▀▀ ░░▀░░ ▀░░▀
+ 
+
+  This is a text based choose your adventure game built in python. You should see your choices (commands) that you can type on the screen.
+
+  I made this as a way to practice what i have learnt till now so you're welcome to see the source code even tho it might be a bit jumbeled up
+  since I didn't finish all the basics yet (after you play it ) .
+  
+  Please open cmd in full screen for the full experience and try to go up the screen in case some text did not fi it.
+
+  HIT ENTER TO CONTINUE \n\n\n\n\n''')
+
+
+
+while True: 
+  enter = input ("\n> ")
+  if enter == "":
+     print('''
+  "Where am i?"
+   
+  You do not know who you are or where you are.
+
+  "....."
+  "I need to find home"
+
+  
+  HIT ENTER TO CONTINUE\n\n\n\n\n''')
+
+     break
+  
+  else:
+   print("\nI said ENTER\n")
+
+while True :
+  enter = input('\n> ')
+  if enter == "" :
+     break
+
+endings = "e"
+loc = "town square"
+show_location(loc)
+
+while True:
+ 
+ if endings.rfind("dn") > 0 or endings.rfind("nd") > 0 and loc == "wizard corner": 
+      
+      show_location(loc)
+      print(worldrooms["wizard corner"]["actions2"])
+      response = input("\n> ").strip().lower()
+      while True :
+       if response != "run away" :
+        print("you cant choose that")
+        response = input("> ")
+      
+       elif response == "run away" :
+          print()
+          print (''' Wizard: "You can't. You can't run away again." 
+                   
+                   > huh?
+                                                                 ''')
+          reply = input("\n> ").strip().lower()
+          while True : 
+           if reply == "huh?" or "huh" :
+              print('"', 'HAHA!'*3500 , '"' ,'\n\n')
+              while True:
+               answer = input("HIT ENTER\n> ")
+               if answer == "" : 
+                 break
+              print(vis[1])
+              quit()
+       
+       
+        
+       
+          
+ response = input("\n> ").strip().lower()
+ 
+ if response == "wizard corner" and loc == "town square" :
+    movedirection(response)
+    
+ elif loc == "town square" and response == "look paper" : 
+     print ('''
+                                               ╔═══════════════════════════════╗
+                                                Only in death are humans equal.
+                                               ╚═══════════════════════════════╝
+ "Where have i seen this before?"
+
+     ''')
+ 
+ 
+ elif loc == "wizard corner" :
+        
+    if response == "can you help me find home?" :
+        print('\n Wizard: "Where is home?"\n ')
+        print(' "...."\n ')
+
+    elif response == "spade" :
+        print('''\n
+  Wizard: "I see we've got GREED."
+          
+  You are forced to push a big rock with your chest on a mountain for eternity.\n
+            ''')
+        endings = endings + "n"
+        if try_again() == True : 
+            show_location(loc)
+            continue
+
+    elif response == "heart" :
+        print(' \n Wizard: "Interesting...."Treachery". \n\n Your body gets frozen in ice. You remain stuck.\n')
+        endings = endings + "d"
+        if try_again() == True :
+            show_location(loc)
+            continue
+
+ else :
+     print("I do not understand that\n")
